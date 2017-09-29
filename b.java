07d9a43bdf58d217297851a33c79c5a0914433fe
tejaAPI <brahmateja.bollineni@gmail.com>
2017-09-29 18:07:29 +0530

    first commit

---
+++ b.java
@@ -0,0 +1,51 @@
+class a
+{
+	int x;
+	int y;
+	void add()
+	{
+		int sum=x+y;
+		
+	}
+	void diff()
+	{
+		int sub=x-y;
+		
+	}
+	void product()
+	{
+		int multiply=x*y;
+		
+	}
+	void divison()
+	{
+		int div=x/y;
+		
+		
+	}
+}
+class b extends a
+{
+	void display()
+	{
+		System.out.println("sum="+sum);
+		System.out.println("diff="+sub);
+		System.out.println("product="+multiply);
+		System.out.println("divison="+div);
+		
+	}
+}
+class single
+{
+	public static void main(String args[])
+	{
+		b obj=new b();
+		obj.x=2;
+		obj.y=3;
+		obj.add();
+		obj.diff();
+		obj.product();
+		obj.divison();
+		obj.display();
+	}
+}
